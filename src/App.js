b55386898c817ed6bd1596d97b88ea562dad828d
botechia-erika <mendoncabotechia@proton.me>
2023-08-12 11:39:53 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <nav class="navbar">Navbar</nav>
+      <div class="container">
+        <section class="s1">Seccion 1</section>
+        <section class="s2">Seccion 2</section>
+        <section class="s3">Seccion 3</section>
+      </div>
+      <footer class="footer">Footer</footer>
+    </div>
+  );
+}
